8d8a173bb5ae5dacf7da47107ac372cf99634a0c
DmitriyMartynenka <dmitriymartyn9797@mail.ru>
2021-11-25 16:16:29 +0300

    add fileUploading, static

--- src/main/java/com/example/sweater/config/MvcConfig.java
+++ src/main/java/com/example/sweater/config/MvcConfig.java
@@ -1,14 +1,24 @@
 package com.example.sweater.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**").addResourceLocations("file://"+uploadPath+"/");
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 }
--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -4,18 +4,26 @@ import com.example.sweater.domain.Message;
 import com.example.sweater.domain.User;
 import com.example.sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class MainController {
     @Autowired
     private MessageRepo messageRepo;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting(Map<String, Object> model) {
@@ -40,8 +48,21 @@ public class MainController {
             @AuthenticationPrincipal User user,
             @RequestParam String text,
             @RequestParam String tag,
-            Map<String, Object> model) {
+            @RequestParam(name = "file", required = false) MultipartFile file,
+            Map<String, Object> model) throws IOException {
         Message message = new Message(text, tag, user);
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
+            File fileDir = new File(uploadPath);
+            if (!fileDir.exists()) {
+                fileDir.mkdir();
+            }
+            String uuidFilename = UUID.randomUUID().toString();
+            String resultFilename = uuidFilename + "." + file.getOriginalFilename();
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+
+            message.setFilename(resultFilename);
+        }
+
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
